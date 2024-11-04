b2f4900b1eafca3e383d3a96b942f8e676f5ce7d
User <toma2006.06.11.@gmail.com>
2024-11-04 23:52:03 +0400

    Создано главное окно программы

---
+++ obmen_exchange.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from tkinter import*
+from tkinter import messagebox as mb
+
+
+
+window=Tk()
+window.title('КУрсы обмена валюты')
+window.geometry("360x180")
+
+Label(text="Введите код валюты").pack(padx=10, pady=10)
+
+
+entry=Enrty()
+entry.pack(padx=10, pady=10)
+
+Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
+
+window.mainloop()
