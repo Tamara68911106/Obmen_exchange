128c7fadc9b24250732262cda81f96ac84bcadd6
User <toma2006.06.11.@gmail.com>
2024-11-08 14:00:02 +0400

    Добавлен второй выпадающий список для выбора базовой валюты

--- obmen_exchange.py
+++ obmen_exchange.py
@@ -6,25 +6,29 @@ from tkinter import ttk
 
 
 def update_c_label(event):
-    code = combobox.get()
+    code = t_combobox.get()
     name = cur[code] #кур был списком, будет словарем. из него будеи=м брать по коду валюты соотвествующее значение.
     c_label.config(text=name)
 
 
 def exchange():
-    code = combobox.get()
+    t_code = t_combobox.get()
+    b_code = b_combobox.get()
 
-    if code:
+
+    if t_code and b_code: # сравниваем 2 условия, если t-code не пустой и b_code не пустой.
+        # если пользователь выбрал 1-й, 2-й, то условие сработает, мы попадаем в try
         try:
-            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response = requests.get(f'https://open.er-api.com/v6/latest/{b_code}')
             response.raise_for_status()
             data = response.json() # раскладываем в виде питонского словаря
-            if code in data['rates']:# проверяем существует ли данная валюта
-                exchange_rate = data['rates'][code]
-                c_name = cur[code] # в перемнную с-name из словаря - значение по ключу
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f}{c_name} за 1 доллар")
+            if t_code in data['rates']:# проверяем существует ли данная валюта
+                exchange_rate = data['rates'][t_code]
+                t_name = cur[t_code] # в перемнную с-name из словаря - значение по ключу
+                b_name = cur[b_code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f}{t_name} за 1 {b_name}")
             else:
-                mb.showerror('Ошибка!', f"Валюта {code} не найдена")# обработка исключений
+                mb.showerror('Ошибка!', f"Валюта {t_code} не найдена")# обработка исключений
         except Exception as e:
             mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
     else:
@@ -42,19 +46,24 @@ cur = {  # используем словать, поэтому {
     'CHF': 'Швейцарский франк',
     'AED': 'Дирхам ОАЭ',
     'CAD': 'Канадский доллар',
+    'USD': 'Американский доллар'
 }
 
 window=Tk()
 window.title('Курсы обмена валюты')
-window.geometry("360x180")
+window.geometry("360x300")
 
 
-Label(text="Выберите код валюты").pack(padx=10, pady=10)
+Label(text="Базовая валюта").pack(padx=10, pady=10)
+b_combobox = ttk.Combobox(values=list(cur.keys()))
+b_combobox.pack(padx=10, pady=10)
 
 
-combobox = ttk.Combobox(values=list(cur.keys()))
-combobox.pack(padx=10, pady=10)
-combobox.bind("<<ComboboxSelected>>", update_c_label)
+Label(text="Целевая валюта").pack(padx=10, pady=10)
+
+t_combobox = ttk.Combobox(values=list(cur.keys())) # cur - словарь, keys - ключи
+t_combobox.pack(padx=10, pady=10)
+t_combobox.bind("<<ComboboxSelected>>", update_c_label)
 
 # entry=Entry  выключаем поле ввода
 # entry.pack(padx=10, pady=10)
@@ -62,6 +71,6 @@ c_label=ttk.Label()
 c_label.pack(padx=10, pady=10)
 
 
-Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
+Button(text="Получить курс обмена", command=exchange).pack(padx=10, pady=10)
 
 window.mainloop()
